a7dfd75e7efcc9d6f9c1f4d1643215b38a453d51
Andrei Neagu <andrei-daniel.neagu@student.unitbv.ro>
2025-02-01 15:02:31 +0200

    added pages

--- App.js
+++ App.js
@@ -11,7 +11,11 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="UniTBv_App">
         <Stack.Screen name="UniTBv_App" component={UniTBv_App} options={{ headerShown: false }} />
-        <Stack.Screen name="DashboardBS" component={DashboardBS} options={{ headerTitle: 'BlueStreamline', headerShown: false }} />
+        <Stack.Screen name="DashboardBS" component={DashboardBS} options={{  headerShown: false }} />
+        <Stack.Screen name="RecruitsBS" component={RecruitsBS} options={{ headerShown: false }} />
+        <Stack.Screen name="HistoryBS" component={HistoryBS} options={{ headerShown: false }} />
+        <Stack.Screen name="TestimonialsBS" component={TestimonialsBS} options={{ headerShown: false }} />
+        <Stack.Screen name="CrewBS" component={CrewBS} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
